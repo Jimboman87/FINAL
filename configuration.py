f8bd6ff620fb498a0d7d6515f634b0545347a43b
Roman Motyzhov <jomon@yandex.ru>
2023-12-17 17:41:53 +0400

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Роман Мотыжов, 11-я когорта - Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://e1f1852f-0bff-40e1-aae9-6e766a3a75e6.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
